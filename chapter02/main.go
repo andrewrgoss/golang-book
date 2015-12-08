5367d6361f14d327e02b4e5ca1574053f5a46cec
Andrew Goss <andrewrgoss@gmail.com>
2015-12-08 10:47:51 -0500

    Added additional comment

--- chapter02/main.go
+++ chapter02/main.go
@@ -7,7 +7,7 @@ There are two types of Go programs: executables and libraries. Executable applic
 import "fmt" // fmt = shorthand for 'format'
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Hello world") // sends whatever you give it to standard output (in the terminal you're working in)
 }
 
 /*Functions are the building blocks of a Go program. They have inputs, outputs and a series of steps called statements which are executed in order. All functions start with the keyword func followed by the name of the function (main in this case), a list of zero or more “parameters” surrounded by parentheses, an optional return type and a “body” which is surrounded by curly braces. This function has no parameters, doesn't return anything and has only one statement. The name main is special because it's the function that gets called when you execute the program.*/
